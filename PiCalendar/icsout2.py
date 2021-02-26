334b571e51cb8e615265e2123da9de3227ac103e
Aakriti Sharma <aakriti01as@gmail.com>
2021-02-26 21:37:47 +0530

    Feat: Read multiple events
    - Function to read multiple events
    - Add testcases

--- PiCalendar/icsout2.py
+++ PiCalendar/icsout2.py
@@ -4,56 +4,70 @@ from datetime import date, datetime, timedelta
 def read_ics(path: str):
     """Read files with extension .ics and return a dictionary.
     :param path: str, path for the .ics file
-    :return: dict
+    :return: list of dictionaries
     """
+    events = list()
     ics_dict = dict()
     with open(path, 'r') as file:
         for line in file:
             key, value = line.strip().split(":")
             ics_dict[key] = value
-    return ics_dict
+            if key=='END':
+                events.append(ics_dict)
+                ics_dict = dict()
+    return events[:-1]
 
 
-def event_dates(start, end, rules):
+def event_dates(start, end, rules=None):
     """Returns list of dates between start and end with rules being followed
     :param start: datetime, start date
     :param end: datetime, end date
     :param rules: str, string of rules separated by ;
     :return: list of valid dates
     """
-    days = {"MO": 0, "TU": 1, "WE": 2, "TH": 3, "FRI": 4, "SA": 5, "SU": 6}
+    days = {"MO": 0, "TU": 1, "WE": 2, "TH": 3, "FR": 4, "SA": 5, "SU": 6}
     date = start
     dates = []
-
-    rules = rules.split(";")
-    rules_dict = dict()
-    for rule in rules:
-        key, value = rule.split("=")
-        rules_dict[key] = value
-    
-    byday = rules_dict['BYDAY'][:2]
-    until = datetime.strptime(rules_dict['UNTIL'], "%Y%m%dT%H%M%S")
-    end = min(end, until)
-    
-    date += timedelta(days = days[byday] - date.weekday())
-    while date <= end:
-        dates.append(date)
-        date += timedelta(days=7)
-    
+    if rules:
+        rules = rules.split(";")
+        rules_dict = dict()
+        for rule in rules:
+            key, value = rule.split("=")
+            rules_dict[key] = value
+        
+        byday = rules_dict['BYDAY'][:2]
+        until = datetime.strptime(rules_dict['UNTIL'], "%Y%m%dT%H%M%S")       
+        date += timedelta(days = days[byday] - date.weekday())
+        while date <= until:
+            dates.append(date)
+            date += timedelta(days=7)
+    else:
+        dates.append(date) 
     return dates
 
   
-def print_calendar(date, start_time, end_time, summary, location):
+def event_details(start_time, end_time, summary, location):
     """Prints the output
     """
-    date = datetime.strftime(date, "%B %d, %Y (%a)")
     start_time = datetime.strftime(start_time, "%I:%M %p")
     end_time = datetime.strftime(end_time, "%I:%M %p")
     location = "{{" + location + "}}"
     
-    print(f"{date}")
-    print('-' * len(date))
-    print(f"{start_time} to {end_time}: {summary} {location}\n")
+    return f"{start_time} to {end_time}: {summary} {location}\n"
+
+def print_calendar(calendar, start, end):
+    """Print the events in calendar is proper format.
+    :param calendar: 
+    """
+    for date in sorted(calendar.keys()):
+        if date < start.date():
+            continue
+        if date > end.date():
+            break
+        date_str = datetime.strftime(date, "%B %d, %Y (%a)")
+        print(date_str)
+        print("-" * len(date_str))
+        print("\n".join(calendar[date]))
 
 
 def icsout(data: dict):
@@ -63,18 +77,26 @@ def icsout(data: dict):
     start_date = datetime.strptime(data['start'], "%Y/%m/%d")
     end_date = datetime.strptime(data['end'], "%Y/%m/%d")
     
-    ics_data = read_ics(data['file'])
-    event_start = datetime.strptime(ics_data['DTSTART'], "%Y%m%dT%H%M%S")
-    event_end = datetime.strptime(ics_data['DTEND'], "%Y%m%dT%H%M%S")
-    
-    start = max(start_date, event_start)
-
-    events = [start]
-    if 'RRULE' in ics_data:
-        events = event_dates(start, end_date, ics_data['RRULE'])
-    
+    events = read_ics(data['file'])    
+    calendar = dict()
+       
     for event in events:
-        print_calendar(event, event_start, event_end, ics_data['SUMMARY'], ics_data['LOCATION'])
+        event_start = datetime.strptime(event['DTSTART'], "%Y%m%dT%H%M%S")
+        event_end = datetime.strptime(event['DTEND'], "%Y%m%dT%H%M%S")
+        
+        start = max(start_date, event_start)
+
+        dates = event_dates(start, end_date, event.get('RRULE'))
+        
+        info = event_details(event_start, event_end, event['SUMMARY'], event['LOCATION'])
+
+        for date in dates:
+            date = date.date()
+            if date in calendar:
+                calendar[date].append(info)
+            else:
+                calendar[date] = [info]
+    print_calendar(calendar, start_date, end_date)
 
 
 if __name__=="__main__":
